3c857fecfb1ca7899694b626d70e85727203e4f9
Chauncey09 <33027599+chauncey09@users.noreply.github.com>
2020-03-06 12:00:16 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,40 +9,40 @@
 sed -i 's/192.168.1.1/192.168.2.10/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. 自定义插件
-cd /openwrt/package/lean
+#cd /openwrt/package/lean
 
-echo "koolproxyR"
-if [ ! -d "./koolproxyR" ];then
-git clone https://github.com/tzxiaozhen88/koolproxyR.git
-else
-cd koolproxyR
-git pull
-cd ..
-fi
+#echo "koolproxyR"
+#if [ ! -d "./koolproxyR" ];then
+#git clone https://github.com/tzxiaozhen88/koolproxyR.git
+#else
+#cd koolproxyR
+#git pull
+#cd ..
+#fi
 
-echo "luci-app-adguardhome"
-if [ ! -d "./luci-app-adguardhome" ];then
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
-else
-cd luci-app-adguardhome
-git pull
-cd ..
-fi
+#echo "luci-app-adguardhome"
+#if [ ! -d "./luci-app-adguardhome" ];then
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+#else
+#cd luci-app-adguardhome
+#git pull
+#cd ..
+#fi
 
-echo "luci-app-mentohust"
-if [ ! -d "./luci-app-mentohust" ];then
-git clone https://github.com/BoringCat/luci-app-mentohust.git
-else
-cd luci-app-mentohust
-git pull
-cd ..
-fi
+#echo "luci-app-mentohust"
+#if [ ! -d "./luci-app-mentohust" ];then
+#git clone https://github.com/BoringCat/luci-app-mentohust.git
+#else
+#cd luci-app-mentohust
+#git pull
+#cd ..
+#fi
 
-echo "luci-app-mentohust Depends"
-if [ ! -d "./MentoHUST-OpenWrt-ipk" ];then
-git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git
-else
-cd MentoHUST-OpenWrt-ipk
-git pull
-cd ..
-fi
+#echo "luci-app-mentohust Depends"
+#if [ ! -d "./MentoHUST-OpenWrt-ipk" ];then
+#git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git
+#else
+#cd MentoHUST-OpenWrt-ipk
+#git pull
+#cd ..
+#fi
